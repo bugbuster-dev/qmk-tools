c2d45ec3ba31bac55e6b6e9fd39884619693c887
bug buster <bugbuster@bugbuster.dev>
2026-05-27 16:14:31 +0200

    fix: use --g short form for ls group-directories-first

--- qmk/QMKata/kbsm_module_examples/kbsm_holdseq/holdseq_def.h
+++ qmk/QMKata/kbsm_module_examples/kbsm_holdseq/holdseq_def.h
@@ -37,7 +37,7 @@ static const holdseq_def_t module_holds[] = {
     { '/', "ht",  "history | grep " },
     { '/', "tx",  "tar xzf " },
     { '/', "gr",  "grep -rn --include=\"*.c\" " },
-    { '/', "ls",  "ls -lah --group-dirs=first" },
+    { '/', "ls",  "ls -lah --g " },
 };
 #define MODULE_HOLDSEQ_COUNT (sizeof(module_holds) / sizeof(module_holds[0]))
 
