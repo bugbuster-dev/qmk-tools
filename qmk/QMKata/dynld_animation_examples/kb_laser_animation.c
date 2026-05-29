5032cc109fcb51b4c477c0b406ceefc8f52212f2
bug buster <bugbuster@bugbuster.dev>
2026-05-29 17:53:43 +0200

    add blue laser R→L on different random row

--- qmk/QMKata/dynld_animation_examples/kb_laser_animation.c
+++ qmk/QMKata/dynld_animation_examples/kb_laser_animation.c
@@ -3,9 +3,12 @@
 #include "dynld_func.h"
 
 #define LASER_LENGTH 8
-#define LASER_R 255
-#define LASER_G 0
-#define LASER_B 0
+#define RED_R 255
+#define RED_G 0
+#define RED_B 0
+#define BLUE_R 0
+#define BLUE_G 0
+#define BLUE_B 255
 
 static inline uint8_t _lcg(uint32_t *s) {
     *s = *s * 1103515245 + 12345;
@@ -15,14 +18,21 @@ static inline uint8_t _lcg(uint32_t *s) {
 __attribute__((section(".text.entry")))
 bool effect_runner_func(dynld_custom_animation_env_t *anim_env,
                          effect_params_t *params) {
-    static uint8_t row;
-    static uint8_t pos;
+    static uint8_t row_r;
+    static uint8_t pos_r;
+    static uint8_t row_b;
+    static uint8_t pos_b;
     static uint32_t rng;
 
     if (params->init) {
         rng = anim_env->time;
-        row = _lcg(&rng) % MATRIX_ROWS;
-        pos = 0;
+        row_r = _lcg(&rng) % MATRIX_ROWS;
+        pos_r = 0;
+        row_b = _lcg(&rng) % MATRIX_ROWS;
+        while (row_b == row_r) {
+            row_b = _lcg(&rng) % MATRIX_ROWS;
+        }
+        pos_b = MATRIX_COLS - 1;
     }
 
     for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
@@ -30,22 +40,41 @@ bool effect_runner_func(dynld_custom_animation_env_t *anim_env,
     }
 
     for (uint8_t c = 0; c < MATRIX_COLS; c++) {
-        uint8_t led = anim_env->led_config->matrix_co[row][c];
+        uint8_t led = anim_env->led_config->matrix_co[row_r][c];
         if (led == 255) continue;
 
-        uint8_t dist = (c <= pos) ? (pos - c) : (MATRIX_COLS - pos + c);
+        uint8_t dist = (c <= pos_r) ? (pos_r - c) : (MATRIX_COLS - pos_r + c);
         if (dist < LASER_LENGTH) {
             uint8_t intensity = (LASER_LENGTH - dist) * 255 / LASER_LENGTH;
             anim_env->set_color(led,
-                (uint8_t)((LASER_R * intensity) >> 8),
-                (uint8_t)((LASER_G * intensity) >> 8),
-                (uint8_t)((LASER_B * intensity) >> 8));
+                (uint8_t)((RED_R * intensity) >> 8),
+                (uint8_t)((RED_G * intensity) >> 8),
+                (uint8_t)((RED_B * intensity) >> 8));
         }
     }
 
-    pos = (pos + 1) % MATRIX_COLS;
-    if (pos == 0) {
-        row = _lcg(&rng) % MATRIX_ROWS;
+    for (uint8_t c = 0; c < MATRIX_COLS; c++) {
+        uint8_t led = anim_env->led_config->matrix_co[row_b][c];
+        if (led == 255) continue;
+
+        uint8_t dist = (c >= pos_b) ? (c - pos_b) : (c + MATRIX_COLS - pos_b);
+        if (dist < LASER_LENGTH) {
+            uint8_t intensity = (LASER_LENGTH - dist) * 255 / LASER_LENGTH;
+            anim_env->set_color(led,
+                (uint8_t)((BLUE_R * intensity) >> 8),
+                (uint8_t)((BLUE_G * intensity) >> 8),
+                (uint8_t)((BLUE_B * intensity) >> 8));
+        }
+    }
+
+    pos_r = (pos_r + 1) % MATRIX_COLS;
+    if (pos_r == 0) {
+        row_r = _lcg(&rng) % MATRIX_ROWS;
+    }
+
+    pos_b = (pos_b + MATRIX_COLS - 1) % MATRIX_COLS;
+    if (pos_b == MATRIX_COLS - 1) {
+        row_b = _lcg(&rng) % MATRIX_ROWS;
     }
 
     return false;
