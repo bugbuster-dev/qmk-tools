abb66f451397e0648a2960bc6fe486cfbf150bf5
bug buster <bugbuster@bugbuster.dev>
2026-06-14 20:12:58 +0200

    fix(rgb): correct angle units in math helpers

--- qmk/QMKata/dynld_animation_examples/kb_murmuration.c
+++ qmk/QMKata/dynld_animation_examples/kb_murmuration.c
@@ -3,12 +3,16 @@
 #include "dynld_func.h"
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.1415926535f
+#endif
+
 static inline int16_t q_sin(dynld_custom_animation_env_t *env, int16_t theta) {
-    return (int16_t)(sinf((float)theta / 256.0f) * 256.0f);
+    return (int16_t)(sinf((float)theta * (2.0f * M_PI / 65536.0f)) * 256.0f);
 }
 
 static inline int16_t q_cos(dynld_custom_animation_env_t *env, int16_t theta) {
-    return (int16_t)(cosf((float)theta / 256.0f) * 256.0f);
+    return (int16_t)(cosf((float)theta * (2.0f * M_PI / 65536.0f)) * 256.0f);
 }
 
 typedef struct {
